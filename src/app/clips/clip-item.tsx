35efdcc69d346517bdd3f37a0c209167130235dc
VELEZ SVS <santy124velez@gmail.com>
2026-01-21 04:44:33 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 're

--- src/app/clips/clip-item.tsx
+++ src/app/clips/clip-item.tsx
@@ -1,5 +1,4 @@
 'use client';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { PostData } from "@/types/post";
 import { AppUser } from "@/types/user";
 import { Avatar, Box, Button, Flex, Text } from "@radix-ui/themes";
