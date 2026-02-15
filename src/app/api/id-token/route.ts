49f7e6233ea3fa834eb9f5a4b6036f98cd1ef12b
VELEZ SVS <santy124velez@gmail.com>
2026-02-15 10:07:08 +0000

    bro

--- src/app/api/id-token/route.ts
+++ src/app/api/id-token/route.ts
@@ -12,7 +12,7 @@ export async function POST(req: NextRequest) {
       httpOnly: true,
       path: "/",
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "lax",
       maxAge: 60 * 60, // 1 hora
     });
 
