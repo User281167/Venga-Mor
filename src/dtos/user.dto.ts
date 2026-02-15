6130830db5bec7e9d4d8dc7d1990b4b691ceb8f5
VELEZ SVS <santy124velez@gmail.com>
2026-02-15 13:42:30 +0000

    Try fixing this error: `Console Error: . Error source: src/lib/apiFetch.

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const UpdateUserInfoSchema = z.object({
   nombre: z.string().min(3).max(30),
   apellido: z.string().optional(),
-  foto: z.string().url().optional(),
+  foto: z.string().url().optional().nullable(),
   descripcion: z.string().optional(),
 });
 
