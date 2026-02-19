94380c56ac496c133c67e66a61772f478e6abbed
Juan <juandiego.velez@utp.edu.co>
2026-02-18 21:28:02 -0500

    Evitar cargar solo videos

--- src/app/clips/page.tsx
+++ src/app/clips/page.tsx
@@ -4,7 +4,7 @@ import { FeedList } from "./components/feed-list";
 export default function ClipsPage() {
   return (
     <section className="h-[94dvh] w-full overflow-hidden bg-black">
-      <FeedList limit={10} mediaType="video" />
+      <FeedList limit={10} />
     </section>
   );
 }
